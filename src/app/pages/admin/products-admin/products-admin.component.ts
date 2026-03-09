684360a377d254f6df353e2cc7c7850dd8e563e3
Alexis Cepeda <alexkiller0408@hotmail.com>
2026-03-08 22:29:36 -0500

    qwqwqwq

--- src/app/pages/admin/products-admin/products-admin.component.ts
+++ src/app/pages/admin/products-admin/products-admin.component.ts
@@ -9,6 +9,7 @@ import { Component, ViewChild } from '@angular/core';
 // import {MatTableDataSource, MatSort} from '@angular/material';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatSort, MatSortModule } from '@angular/material/sort';
+import { ProductService } from '../../../services/product.service';
 
 @Component({
   selector: 'app-products-admin',
@@ -24,7 +25,18 @@ import { MatSort, MatSortModule } from '@angular/material/sort';
   styleUrl: './products-admin.component.css'
 })
 export class ProductsAdminComponent {
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+
+  products: any[] = []
+
+  constructor(private productService: ProductService) { }
+  ngOnInit() {
+
+    this.productService.getProducts().subscribe((data: any) => {
+      this.products = data;
+    });
+
+  }
+  displayedColumns = ['position', 'name', 'weight', 'symbol', 'price', 'category'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   @ViewChild(MatSort) sort!: MatSort;
@@ -44,27 +56,29 @@ export interface Element {
   position: number;
   weight: number;
   symbol: string;
+  price: string;
+  category: string;
 }
 
 const ELEMENT_DATA: Element[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' , price: '0,00', category: 'es una categoria'},
+  // { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  // { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  // { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  // { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  // { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  // { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  // { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  // { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  // { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  // { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
+  // { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
+  // { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
+  // { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
+  // { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
+  // { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
+  // { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
+  // { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
+  // { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
+  // { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
 ];
